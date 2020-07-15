971b8a45adebae48239e92f2f0497d35892ade23
manuel carrera <manuel.carrera@workday.com>
2020-07-15 14:19:53 -0600

    fix: Add toId method to support new storybook

--- react.js
+++ react.js
@@ -1,13 +1,47 @@
 import { forceReRender } from '@storybook/react'
 import addons from '@storybook/addons'
 import Events from '@storybook/core-events'
-import { toId } from '@storybook/router'
 import ReactDOM from 'react-dom'
+
+/**
+ * Remove punctuation and illegal characters from a story ID.
+ *
+ * See https://gist.github.com/davidjrice/9d2af51100e41c6c4b4a
+ */
+export const sanitize = (string) => {
+  return (
+    string
+      .toLowerCase()
+      // eslint-disable-next-line no-useless-escape
+      .replace(/[ ’–—―′¿'`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '-')
+      .replace(/-+/g, '-')
+      .replace(/^-+/, '')
+      .replace(/-+$/, '')
+  )
+}
+
+const sanitizeSafe = (string, part) => {
+  const sanitized = sanitize(string)
+  if (sanitized === '') {
+    throw new Error(
+      `Invalid ${part} '${string}', must include alphanumeric characters`
+    )
+  }
+  return sanitized
+}
+
+/**
+ * Generate a storybook ID from a component/kind and story name.
+ * This is a copy from https://github.com/storybookjs/csf/blob/next/src/index.ts
+ * to be able to support storybook 6.x since they moved toId from storybook/router to storybook/csf
+ */
+export const toId = (kind, name) =>
+  `${sanitizeSafe(kind, 'kind')}--${sanitizeSafe(name, 'name')}`
 
 function setCurrentStory(categorization, story) {
   clearCurrentStory()
   addons.getChannel().emit(Events.SET_CURRENT_STORY, {
-    storyId: toId(categorization, story)
+    storyId: toId(categorization, story),
   })
   addons.getChannel().emit('storybookjs/knobs/reset')
 
