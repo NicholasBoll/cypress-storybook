986b4d69a4c7faaecae802d50c7668d836027ad8
Nicholas Boll <nicholas.boll@gmail.com>
2020-12-31 14:27:37 -0700

    Update dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,7 @@ module.exports = {
   addons: [
     '@storybook/addon-actions',
     '@storybook/addon-links',
-    '@storybook/addon-knobs/register'
-  ]
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-actions/register',
+  ],
 }
